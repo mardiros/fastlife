77733d93a56d62740a6992dca0d5711420a447c7
Guillaume Gauvrit <guillaume@gauvr.it>
2024-08-10 14:14:00 +0200

    Monkey patch until jinjax #85 is fixed

--- src/fastlife/__init__.py
+++ src/fastlife/__init__.py
@@ -1,8 +1,11 @@
 from .configurator import Configurator, configure
 from .configurator.registry import Registry
 
+from .monkeypatch import monkeypatch
 # from .request.form_data import model
 from .templating import Template, template
+
+monkeypatch()
 
 __all__ = [
     # Config
