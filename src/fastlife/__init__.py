680ee679d37d651b5f3fd0c2ed33139741f7f55c
Guillaume Gauvrit <guillaume@gauvr.it>
2024-08-11 19:22:22 +0200

    Update JinjaX

--- src/fastlife/__init__.py
+++ src/fastlife/__init__.py
@@ -1,11 +1,8 @@
 from .configurator import Configurator, configure
 from .configurator.registry import Registry
 
-from .monkeypatch import monkeypatch
 # from .request.form_data import model
 from .templating import Template, template
-
-monkeypatch()
 
 __all__ = [
     # Config
--- src/fastlife/monkeypatch.py
+++
@@ -1,8 +0,0 @@
-import re
-from jinjax import component
-
-
-def monkeypatch() -> None:
-    # the current regex is currently breaking the A component
-    # and we don't use comments inside {# def #}
-    component.RX_INTER_COMMENTS = re.compile("^$")
