b8ab7169f44fd06aa4bf07511a38e4b118c2aa1f
Guillaume Gauvrit <guillaume@gauvr.it>
2024-08-12 10:00:22 +0200

    Fist test name

--- tests/unittests/app/views/test_icons.py
+++ tests/unittests/app/views/test_icons.py
@@ -1,6 +1,6 @@
 from fastlife.testing import WebTestClient
 
 
-def test_show_widget_builtins_str(client: WebTestClient):
+def test_show_icons_respond_200(client: WebTestClient):
     resp = client.get("/icons")
     assert resp.status_code == 200
