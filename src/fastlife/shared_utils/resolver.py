5e7d78308c0482cfdc42b2f2dc591988938991a4
Guillaume Gauvrit <guillaume@gauvr.it>
2024-10-06 16:17:07 +0200

    Implement relative to parent and use a new function for that

--- src/fastlife/shared_utils/resolver.py
+++ src/fastlife/shared_utils/resolver.py
@@ -1,6 +1,7 @@
 """Resolution of python objects for dependency injection and more."""
 
 import importlib.util
+import inspect
 from pathlib import Path
 from types import ModuleType, UnionType
 from typing import Any, Union
@@ -72,3 +73,38 @@ def resolve_package(mod: ModuleType) -> ModuleType:
     parent_module_name = mod.__name__.rsplit(".", 1)[0]
     parent_module = importlib.import_module(parent_module_name)
     return parent_module
+
+
+def _strip_left_dots(s: str) -> tuple[str, int]:
+    stripped_string = s.lstrip(".")
+    num_stripped_dots = len(s) - len(stripped_string)
+    return stripped_string, num_stripped_dots - 1
+
+
+def _get_parent(pkg: str, num_parents: int) -> str:
+    if num_parents == 0:
+        return pkg
+    segments = pkg.split(".")
+    return ".".join(segments[:-num_parents])
+
+
+def resolve_maybe_relative(mod: str, stack_depth: int = 1) -> ModuleType:
+    """
+    Resolve a module, maybe relative to the stack frame and import it.
+
+    :param mod: the module to import. starts with a dot if it is relative.
+    :param stack_depth: relative to which module in the stack.
+        used to do an api that call it instead of resolve the module directly.
+    :return: the imported module
+    """
+    if mod.startswith("."):
+        caller_module = inspect.getmodule(inspect.stack()[stack_depth][0])
+        # we could do an assert here but caller_module could really be none ?
+        parent_module = resolve_package(caller_module)  # type: ignore
+        package = parent_module.__name__
+        mod, count = _strip_left_dots(mod)
+        package = _get_parent(package, count)
+        mod = f"{package}.{mod}"
+
+    module = importlib.import_module(mod)
+    return module
