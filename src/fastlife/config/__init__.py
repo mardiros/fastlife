ea4d5ea52a1affdd2c66dc823759d6f86ef5adab
Guillaume Gauvrit <guillaume@gauvr.it>
2024-11-09 09:48:52 +0100

    Export type

--- src/fastlife/config/__init__.py
+++ src/fastlife/config/__init__.py
@@ -1,7 +1,7 @@
 """Configure fastlife app for dependency injection."""
 
 from .configurator import Configurator, GenericConfigurator, configure
-from .registry import DefaultRegistry
+from .registry import DefaultRegistry, GenericRegistry
 from .resources import resource, resource_view
 from .settings import Settings
 from .views import view_config
@@ -13,6 +13,7 @@ __all__ = [
     "view_config",
     "resource",
     "resource_view",
+    "GenericRegistry",
     "DefaultRegistry",
     "Settings",
 ]
