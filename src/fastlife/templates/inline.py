d41dfdf3291527367900392e06398c62989d4e88
Guillaume Gauvrit <guillaume@gauvr.it>
2024-11-09 09:45:33 +0100

    Update typing for the regitry

--- src/fastlife/templates/inline.py
+++ src/fastlife/templates/inline.py
@@ -15,6 +15,7 @@ class InlineTemplate(BaseModel):
     They use a component lirary to stay small but contains a view logic
     tighly coupled with the view and its code can stay in the same module of that view.
     """
+
     model_config = ConfigDict(arbitrary_types_allowed=True)
 
     template: ClassVar[str]
