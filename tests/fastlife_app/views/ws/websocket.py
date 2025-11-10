245065bcc5855e19b71c2a4b0791b9160d5629b1
Guillaume Gauvrit <guillaume@gauvr.it>
2025-11-10 21:47:41 +0100

    Implement the registry in the websocket

--- tests/fastlife_app/views/ws/websocket.py
+++ tests/fastlife_app/views/ws/websocket.py
@@ -1,8 +1,40 @@
-from fastlife import WebSocket, websocket_view
+from starlette.responses import JSONResponse
+
+from fastlife import websocket_view
+from fastlife.adapters.fastapi.websocket import GenericWebSocket
 
 
-@websocket_view("ws", "/ws")
-async def ws(sock: WebSocket):
+@websocket_view("ws", "/ws/json")
+async def ws_json(sock: GenericWebSocket):
     await sock.accept()
-    await sock.send_json({"msg": "Hello WebSocket"})
+    await sock.send_json({"msg": f"Hello {sock.registry.settings.domain_name}"})
+    new_message = await sock.receive_json()
+    await sock.send_json(new_message)
+    await sock.close()
+
+
+@websocket_view("ws", "/ws/text")
+async def ws_text(sock: GenericWebSocket):
+    await sock.accept()
+    await sock.send_text(f"Hello {sock.registry.settings.domain_name}")
+    new_message = await sock.receive_text()
+    await sock.send_text(new_message)
+    await sock.close()
+
+
+@websocket_view("ws", "/ws/bytes")
+async def ws_bytes(sock: GenericWebSocket):
+    await sock.accept()
+    await sock.send_bytes(f"Hello {sock.registry.settings.domain_name}".encode())
+    new_message = await sock.receive_bytes()
+    await sock.send_bytes(new_message)
+    await sock.close()
+
+
+@websocket_view("ws", "/ws/denied")
+async def ws_denied(sock: GenericWebSocket):
+    await sock.accept()
+    await sock.send_denial_response(
+        JSONResponse({"error": "invalid_token"}, status_code=403)
+    )
     await sock.close()
--- tests/unittests/app/test_websocket.py
+++ tests/unittests/app/test_websocket.py
@@ -8,7 +8,28 @@ def wsclient(app: FastAPI):
     return TestClient(app, headers={"Authorization": "Bearer abc"})
 
 
-def test_websocket(wsclient: TestClient):
-    with wsclient.websocket_connect("/ws") as websocket:
+def test_websocket_json(wsclient: TestClient):
+    with wsclient.websocket_connect("/ws/json") as websocket:
         data = websocket.receive_json()
-        assert data == {"msg": "Hello WebSocket"}
+        assert data == {"msg": "Hello testserver.local"}
+        websocket.send_json({"msg": "Hello from test"})
+        data = websocket.receive_json()
+        assert data == {"msg": "Hello from test"}
+
+
+def test_websocket_text(wsclient: TestClient):
+    with wsclient.websocket_connect("/ws/text") as websocket:
+        data = websocket.receive_text()
+        assert data == "Hello testserver.local"
+        websocket.send_text("Hello from test")
+        data = websocket.receive_text()
+        assert data == "Hello from test"
+
+
+def test_websocket_bytes(wsclient: TestClient):
+    with wsclient.websocket_connect("/ws/bytes") as websocket:
+        data = websocket.receive_bytes()
+        assert data == b"Hello testserver.local"
+        websocket.send_bytes(b"Hello from test")
+        data = websocket.receive_bytes()
+        assert data == b"Hello from test"
