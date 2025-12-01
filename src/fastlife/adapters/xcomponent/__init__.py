db9ba50cc034979383e68b3b803f2db0b9f0cf8a
Guillaume Gauvrit <guillaume@gauvr.it>
2025-12-01 20:39:35 +0100

    Add missing docstring

--- src/fastlife/adapters/xcomponent/__init__.py
+++ src/fastlife/adapters/xcomponent/__init__.py
@@ -1,1 +1,8 @@
+"""
+Adapt the XComponent template engine for fastlife.
 
+It provide a list of html component ready to used and customizable,
+and a few functions.
+
+The pydantic_form bring form fieldset generation from pydantic base models.
+"""
