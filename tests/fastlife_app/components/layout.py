9b5d6aef39cc4b7e3cb5ddfb889907fe8d819e73
Guillaume Gauvrit <guillaume@gauvr.it>
2025-12-20 13:51:45 +0100

    Create a layout catalog for the test

--- tests/fastlife_app/components/layout.py
+++ tests/fastlife_app/components/layout.py
@@ -1,28 +1,44 @@
-from xcomponent import XNode
+from xcomponent import Catalog, XNode
 
 from fastlife.adapters.xcomponent.registry import x_component
 
+layout_catalog = Catalog()
 
-@x_component()
-def Layout(children: XNode):
+
+@layout_catalog.component
+def Head():
     return """
-    <>
-    <!DOCTYPE html>
-    <html>
     <head>
         <meta charset="utf-8" />
         <title>test - Fastlife</title>
         <script src="/static/scripts/htmx.2.0.1.min.js" crossorigin="anonymous">{""}</script>
         <link href="/static/css/main.css" rel="stylesheet" />
     </head>
+    """
+
+
+@layout_catalog.component
+def Body():
+    return """
     <body>
         {/* <script>
         htmx.logAll();
         </script> */}
         <div id="maincontent" hx-target="this" hx-swap="innerHTML">
-        { children }
+        {children}
         </div>
     </body>
+    """
+
+
+@x_component(use={"layout": layout_catalog})
+def Layout(children: XNode):
+    return """
+    <>
+    <!DOCTYPE html>
+    <html>
+    <layout.Head />
+    <layout.Body>{children}</layout.Body>
     </html>
     </>
     """
