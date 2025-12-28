9a55e9db4905ab2a4d756049b5d23752ce7b6ac8
Guillaume Gauvrit <guillaume@gauvr.it>
2025-12-28 10:07:45 +0100

    Move tests to appropriate suite

--- tests/unittests/shared_utils/test_infer.py
+++ tests/unittests/shared_utils/test_infer.py
@@ -1,28 +1,52 @@
-from typing import Any, NewType, Union
+from typing import Any, NewType, Optional, Union
 from uuid import UUID
 
 import pytest
+from pydantic import BaseModel
 
-from fastlife.shared_utils.infer import is_newtype, is_union
+from fastlife.shared_utils.infer import is_complex_type, is_newtype, is_union
 
 UserId = NewType("UserId", UUID)
 
 
+class DummyModel(BaseModel):
+    name: str
+
+
 @pytest.mark.parametrize(
-    "type,expected",
+    "typ,expected",
     [
+        pytest.param(bool, False, id="bool"),
         pytest.param(int, False, id="int"),
         pytest.param(str, False, id="str"),
-        pytest.param(int | str, True, id="int | str"),
-        pytest.param(
-            Union[int, str],  # noqa: UP007
-            True,
-            id="Union[int, str]",
-        ),
+        pytest.param(str | int, True, id="str|int"),
+        pytest.param(str | None, True, id="str|None"),
+        pytest.param(DummyModel, True, id="DummyModel"),
+        pytest.param(DummyModel | None, True, id="DummyModel|None"),
+        pytest.param(DummyModel | str, True, id="DummyModel|str"),
     ],
 )
-def test_is_union(type: type[Any], expected: bool):
-    assert is_union(type) is expected
+def test_is_complex_type(typ: type[Any], expected: bool):
+    assert is_complex_type(typ) is expected
+
+
+@pytest.mark.parametrize(
+    "typ,expected",
+    [
+        pytest.param(bool, False, id="bool"),
+        pytest.param(int, False, id="int"),
+        pytest.param(str, False, id="str"),
+        pytest.param(str | int, True, id="str|int"),
+        pytest.param(Union[str, int], True, id="Union[str, int]"),  # noqa: UP007
+        pytest.param(str | None, True, id="str|None"),
+        pytest.param(Optional[str], True, id="Optional[str]"),  # noqa: UP045
+        pytest.param(DummyModel, False, id="DummyModel"),
+        pytest.param(DummyModel | None, True, id="DummyModel|None"),
+        pytest.param(DummyModel | str, True, id="DummyModel|str"),
+    ],
+)
+def test_is_union(typ: type[Any], expected: bool):
+    assert is_union(typ) is expected
 
 
 @pytest.mark.parametrize(
