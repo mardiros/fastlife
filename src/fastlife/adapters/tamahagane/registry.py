326f7005700d55fc7907bd0cca1bca285aca161d
Guillaume Gauvrit <guillaume@gauvr.it>
2026-01-27 23:09:45 +0100

    Add missing docstring

--- src/fastlife/adapters/tamahagane/registry.py
+++ src/fastlife/adapters/tamahagane/registry.py
@@ -1,3 +1,10 @@
+"""
+Tamahagane registry.
+
+Hooks are attached to a category on import, and then the scanner
+will attach a decorated method to the its instanciated configurator.
+"""
+
 from dataclasses import dataclass
 from typing import TYPE_CHECKING, Any
 
@@ -7,7 +14,10 @@ if TYPE_CHECKING:
 
 @dataclass
 class THRegistry:
+    """Hold a category with its associated registry."""
+
     fastlife: "GenericConfigurator[Any]"
+    """The framework registry."""
 
 
 TH_CATEGORY = "fastlife"
